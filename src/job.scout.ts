872f56ff3b3d51c25a9f923096347756dac442ba
drayuka <ct12067@gmail.com>
2017-11-14 16:18:57 -0600

    fix ts bugs

--- src/job.scout.ts
+++ src/job.scout.ts
@@ -12,6 +12,7 @@ var job = require('job');
 var creep = require('creep');
 // can be called with just name, or with target as well
 class ScoutJob extends JobClass {
+    _observers: StructureObserver[];
     execute() {
         var self = this;
         self.reassessScoutingSites();
@@ -19,16 +20,16 @@ class ScoutJob extends JobClass {
         self.updateRequisition();
         self.controlWorkers();
     }
-    removeRoom(roomName) {
+    removeRoom(roomName: string) {
         var self = this;
-        _.forEach(self.goals[roomName].assignments, function (creepName) {
-            self.creeps[creepName].sucide();
+        _.forEach(self.goals[roomName].assignments, function (creepName: string) {
+            self.creeps[creepName].suicide();
         });
         self.removeGoal(roomName);
     }
-    addRoomToScout(roomName) {
+    addRoomToScout(roomName: string) {
         var self = this;
-        var scoutedRooms = _.map(self.goals, function (goal) {
+        var scoutedRooms = _.map(self.goals, function (goal: GoalClass) {
             return goal.roomName;
         });
         if(_.includes(scoutedRooms, roomName)) {
@@ -46,7 +47,7 @@ class ScoutJob extends JobClass {
                 return true;
             }
             var ownedStructures = Game.rooms[goal.roomName].find(FIND_MY_STRUCTURES);
-            if(ownedStructures.length) {
+            if(ownedStructures.length != 0) {
                 self.removeRoom(goal.roomName);
             }
         });
@@ -57,7 +58,18 @@ class ScoutJob extends JobClass {
             if(goal.assignments.length != 0) {
                 return true;
             }
-            self.jobs.spawn.addRequisition(self.name, 'scout', 1, goal.id, {});
+            self.jobs.spawn.addRequisition([{
+                power: 1,
+                type: 'scout',
+                memory: {},
+                id: goal.id,
+                jobName: self.name,
+                // this is a hack so that we don't have to do a 
+                // bunch of logic that we should write anyways to handle global jobs
+                parentClaim: self.name, 
+                waitingSince: Game.time,
+                newClaim: undefined
+            }]);
         });
     }
     get observers () {
@@ -65,19 +77,19 @@ class ScoutJob extends JobClass {
         if(self._observers) {
             return self._observers;
         }
-        self._observers = _.map(self.memory.observers, function (obs) {
-            return Game.getObjectById(obs);
+        self._observers = _.map(self.memory.observers, function (obs: string) {
+            return <StructureObserver>Game.getObjectById(obs);
         });
         return self._observers;
     }
     updateObservers() {
         var self = this;
-        var newObservers = [];
-        _.forEach(global.claimedRooms, function (roomName) {
+        var newObservers: string[] = [];
+        _.forEach(global.bootstrap.claimedRooms, function (room, roomName) {
             if(!Game.rooms[roomName]) {
                 return true;
             }
-            var observer = Game.rooms[roomName].find(FIND_MY_STRUCTURES, {filter: function (struct) {
+            var observer: StructureObserver[] = Game.rooms[roomName].find(FIND_MY_STRUCTURES, {filter: function (struct: OwnedStructure) {
                 return struct.structureType == STRUCTURE_OBSERVER;
             }});
             if(observer.length > 0) {
@@ -90,17 +102,22 @@ class ScoutJob extends JobClass {
     controlWorkers() {
         var self = this;
         _.forEach(self.goals, function (goal) {
-            if(goal.assignments.length == 0 && self.observers.length != 0) {
-                var observer = self.observers.shift();
-                observer.observeRoom(goal.roomName);
-            } else {
-                _.forEach(goal.assignments, function (creepName) {
-                    self.controlCreep(self.creeps[creepName]);
-                })
+            var room = Game.rooms[goal.roomName];
+            if(!room) {
+                var observer = _.find(self.observers, function (obs) {
+                    return Game.map.getRoomLinearDistance(obs.pos.roomName, goal.roomName) < OBSERVER_RANGE;
+                });
+                if(observer) {
+                    observer.observeRoom(goal.roomName);
+                }
             }
-        })
+
+            _.forEach(goal.assignments, function (creepName) {
+                self.controlCreep(self.creeps[creepName]);
+            });
+        });
     }
-    controlCreep(myCreep) {
+    controlCreep(myCreep: CreepClass) {
         var self = this;
         if(myCreep.pos.roomName == myCreep.goal.roomName) {
             if(myCreep.pos.x == 0 || myCreep.pos.x == 49 || myCreep.pos.y == 0 || myCreep.pos.y == 49) {
@@ -109,10 +126,6 @@ class ScoutJob extends JobClass {
                 myCreep.moveOffRoad();
             }
         } else {
-            if(self.observers.length > 0) {
-                var observer = self.observers.shift();
-                observer.observeRoom(myCreep.goal.roomName);
-            }
             myCreep.navigate();
         }
     }
