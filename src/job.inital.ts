89dab50608dc2d1bee1d4f7bcd3bb871dfab1fb1
drayuka <ct12067@gmail.com>
2017-11-28 12:55:31 -0600

    initial work on initial room handling.

---
+++ src/job.inital.ts
@@ -0,0 +1,37 @@
+class InitialRoom extends MissionJobClass {
+	execute () {
+		var self = this;
+		self.generateMissions();
+		self.assignMissions();
+		self.updateRequisitions();
+		self.runMissions();
+	}
+	get missionGenerators() : MissionGenerators {
+		var self = this;
+		return {
+			upgrade: {
+				init: function (missionMem: any) {
+					missionMem.missions = {};
+				},
+				new: function (missionMem: any, rooms: strings[]) : Mission[] {
+					var newMissions : Mission[] = [];
+					_.forEach(rooms, function (roomName) {
+
+					});
+					return newMissions;
+				},
+				remove: function (missionMem: any, mission: Mission) {
+					delete missionMem.missions[mission.other.roomName];
+				},
+				creepMissionInit: function (creep : CreepClass) {
+					creep.memory.missionStatus = {
+						
+					}
+				}
+			}
+		}
+	}
+	updateRequisitions() {
+		var self = this;
+	}
+}
